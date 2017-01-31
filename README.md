8f931d6eedd2a1c5ab9eda4338b73b4616bffa96
Alexander Feiglstorfer <delooks@gmail.com>
2017-01-31 22:49:19 +0100

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+# Vue Storyblok plugin
+
+Make any element editable with the Vue Storyblok directive. Just add ```v-editable="content"``` to a html element.
+
+## License
+
+[MIT](http://opensource.org/licenses/MIT)
---
+++ storyblok-vue.js
@@ -0,0 +1,37 @@
+(function () {
+
+  var addClass = function(el, className) {
+    if (el.classList) {
+      el.classList.add(className)
+    } else if (!hasClass(el, className)) {
+      el.className += ' ' + className
+    }
+  }
+
+  function install(Vue) {
+
+    Vue.directive('touch', {
+      bind: function(el, binding) {
+        if (typeof binding.value._editable === 'undefined') {
+          return
+        }
+
+        var options = JSON.parse(binding.value._editable.replace('<!--#storyblok#', '').replace('-->', ''))
+
+        el.setAttribute('data-blok-c', JSON.stringify(options))
+        el.setAttribute('data-blok-uid', options.id + '-' + options.uid)
+
+        addClass(el, 'storyblok__outline')
+      }
+    }
+  }
+
+  if (typeof exports == "object") {
+    module.exports = install
+  } else if (typeof define == "function" && define.amd) {
+    define([], function(){ return install })
+  } else if (window.Vue) {
+    Vue.use(install)
+  }
+
+})()
