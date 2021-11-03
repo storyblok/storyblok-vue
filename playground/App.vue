240ebe5c8e49bfc203996b0fc469958b74c9e8ef
Alex Jover <alexjovermorales@gmail.com>
2021-11-03 10:10:34 +0100

    chore: update libs and texts

--- playground/App.vue
+++ playground/App.vue
@@ -1,22 +1,18 @@
 <template>
-  <div id="app">
-    <h2>Vue 2</h2>
-    <div v-editable="blok.content">
+  <div>
+    <h2>Vue 3</h2>
+    <div data-test="editable" v-editable="blok.content">
       Open DevTools and inspect this &lt;div&gt;. You should see a
       <i>data-blok-c</i> and <i>data-blok-uid</i> attrs.
     </div>
   </div>
 </template>
 
-<script>
-export default {
-  data: () => ({
-    blok: {
-      content: {
-        _editable: `<!--#storyblok#{ "id": 12345, "uid": "fc34-uad1"}-->`,
-      },
-    },
-  }),
+<script setup>
+const blok = {
+  content: {
+    _editable: `<!--#storyblok#{ "id": 12345, "uid": "fc34-uad1"}-->`,
+  },
 };
 </script>
 
