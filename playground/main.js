5e601680a9885e42eb1a3b4277d0c3b552654628
Alex Jover <alexjovermorales@gmail.com>
2021-11-15 18:40:29 +0100

    fix(workspace): fix merging the next branch

--- playground/main.js
+++ playground/main.js
@@ -1,11 +1,7 @@
-import Vue from "vue";
+import { createApp } from "vue";
 import StoryblokVue from "@storyblok/vue";
 import App from "./App.vue";
 
-Vue.config.productionTip = false;
-
-Vue.use(StoryblokVue);
-
-new Vue({
-  render: (h) => h(App),
-}).$mount("#app");
+const app = createApp(App);
+app.use(StoryblokVue);
+app.mount("#app");
