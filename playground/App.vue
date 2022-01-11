4a0a0f04613955071cb465d9e8f22cedee86e3a7
Alex Jover <alexjovermorales@gmail.com>
2022-01-11 20:14:08 +0100

    chore(playground): update playground for new changes

--- playground/App.vue
+++ playground/App.vue
@@ -3,17 +3,28 @@
     <h2>Vue 3</h2>
     <div data-test="editable" v-editable="blok.content">
       Open DevTools and inspect this &lt;div&gt;. You should see a
-      <i>data-blok-c</i> and <i>data-blok-uid</i> attrs.
+      <i>data-blok-c</i>
+      and
+      <i>data-blok-uid</i>
+      attrs.
     </div>
   </div>
 </template>
 
 <script setup>
+import { onMounted, inject } from "vue";
+import { useStoryblokBridge } from "@storyblok/vue";
+
 const blok = {
   content: {
     _editable: `<!--#storyblok#{ "id": 12345, "uid": "fc34-uad1"}-->`,
   },
 };
+
+const storyblokApi = inject("storyblokApi");
+console.log(storyblokApi);
+
+onMounted(() => useStoryblokBridge(12345, () => console.log("hola")));
 </script>
 
 <style>
--- playground/main.js
+++ playground/main.js
@@ -1,7 +1,15 @@
 import { createApp } from "vue";
-import StoryblokVue from "@storyblok/vue";
 import App from "./App.vue";
+import { StoryblokVue } from "@storyblok/vue";
 
 const app = createApp(App);
-app.use(StoryblokVue);
+
+app.use(StoryblokVue, {
+  options: {
+    api: { accessToken: "lalala" },
+    bridge: true,
+  },
+  use: [], // use it only if you need it
+});
+
 app.mount("#app");
