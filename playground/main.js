805f38bba0c17e981fe8c17c7bf18c934f410711
Alex Jover <alexjovermorales@gmail.com>
2022-01-12 09:50:06 +0100

    test(lib): improve tests

--- playground/main.js
+++ playground/main.js
@@ -7,7 +7,7 @@ const app = createApp(App);
 app.use(StoryblokVue, {
   options: {
     api: { accessToken: "wANpEQEsMYGOwLxwXQ76Ggtt" },
-    // bridge: false
+    bridge: false,
   },
   use: [apiPlugin], // use it only if you need it
 });
