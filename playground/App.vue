eca83f3b2a88f73632fd02e88fd9515ba50fe60d
Alex Jover <alexjovermorales@gmail.com>
2022-01-24 22:59:45 +0100

    test(lib): update tests to new functionalities

--- playground/App.vue
+++ playground/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <div id="app">
-    <h2>Vue 2</h2>
+  <div>
+    <h2>Vue 3</h2>
     <div data-test="editable" v-editable="blok.content">
       Open DevTools and inspect this &lt;div&gt;. You should see a
       <i>data-blok-c</i>
@@ -8,19 +8,30 @@
       <i>data-blok-uid</i>
       attrs.
     </div>
+
+    <h3>
+      <code>storyblokApi.get:</code>
+      <span data-test="api">{{ apiExists }}</span>
+    </h3>
   </div>
 </template>
 
 <script>
+import { useStoryblokBridge } from "@storyblok/vue";
+
 export default {
   data: () => ({
+    apiExists: false,
     blok: {
       content: {
-        // ...
         _editable: `<!--#storyblok#{ "id": 12345, "uid": "fc34-uad1"}-->`,
       },
     },
   }),
+  mounted() {
+    this.apiExists = !!this.$storyblokApi;
+    useStoryblokBridge(12345, () => console.log("hola"));
+  },
 };
 </script>
 
--- playground/main.js
+++ playground/main.js
@@ -1,10 +1,13 @@
 import Vue from "vue";
-import StoryblokVue from "@storyblok/vue";
+import { StoryblokVue, apiPlugin } from "@storyblok/vue";
 import App from "./App.vue";
 
-Vue.config.productionTip = false;
-
-Vue.use(StoryblokVue);
+Vue.use(StoryblokVue, {
+  accessToken: "wANpEQEsMYGOwLxwXQ76Ggtt",
+  // bridge: false,
+  // apiOptions: {  },
+  use: [apiPlugin], // use it only if you need it
+});
 
 new Vue({
   render: (h) => h(App),
