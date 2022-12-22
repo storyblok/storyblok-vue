0d53a6f3667f5a54b76b2197c45c4d247ce5daf4
Dawntraoz <albasilventefu@gmail.com>
2022-12-22 13:50:55 +0100

    fix: cypress issue by commenting SSL

--- playground/main.ts
+++ playground/main.ts
@@ -11,7 +11,7 @@ const app = createApp(App);
 app.use(StoryblokVue, {
   accessToken: "OurklwV5XsDJTIE1NJaD2wtt",
   use: [apiPlugin], // use it only if you need it
-  apiOptions: { https: true },
+  // apiOptions: { https: true },
 });
 
 app.component("Page", Page);
--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import basicSsl from "@vitejs/plugin-basic-ssl";
+// import basicSsl from "@vitejs/plugin-basic-ssl";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), basicSsl()],
+  plugins: [
+    vue(),
+    //, basicSsl()
+  ],
 });
